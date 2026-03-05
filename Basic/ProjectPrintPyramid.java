4985cc4f810d33923231557c30d0e5ad5045bf86
Kameshwaran P <kamesh-13187@kamesh-13187.csez.zohocorpin.com>
2026-03-05 21:51:42 +0530

    7 Pyramid output

--- Basic/ProjectPrintPyramid.java
+++ Basic/ProjectPrintPyramid.java
@@ -4,24 +4,16 @@ public class ProjectPrintPyramid {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int n = sc.nextInt();
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
 
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("      " + num);
+        System.out.println("     " + num + " " + num);
+        System.out.println("    " + num + " " + num + " " + num);
+        System.out.println("   " + num + " " + num + " " + num + " " + num);
+        System.out.println("  " + num + " " + num + " " + num + " " + num + " " + num);
+        System.out.println(" " + num + " " + num + " " + num + " " + num + " " + num + " " + num);
+        System.out.println(num + " " + num + " " + num + " " + num + " " + num + " " + num + " " + num);
 
-            // Print spaces
-            for (int j = 1; j <= 5 - i; j++) {
-                System.out.print(" ");
-            }
-
-            // Print the input number
-            for (int j = 1; j <= i; j++) {
-                System.out.print(n + " ");
-            }
-
-            System.out.println();
-        }
-
-        sc.close();
     }
 }
