98cfb26a09beb4e9f9b549f1c59bed318f73d3ad
kamesh96official <cameociscoz@gmail.com>
2026-03-14 22:08:13 +0530

    else if

--- Basic/IfDemo.java
+++ Basic/IfDemo.java
@@ -12,15 +12,47 @@ public static void main (String args[]){
         System.out.println(num +" " +"The number is positive");
         
     }
-    else{
+    else if (num ==0)
 
-        if( num ==0){
+       {
             System.out.println(num + " "+ "The number is Neutral");
         }
         else{
         System.out.println(num  +" "+ "The number is negative");
         }
+
+        /************** Mark list ********************
+                        >=90 --A Grade
+                        80 - 89 -- B Grade
+                        70 - 79 -- C Grade
+                        60 - 69 -- D Grade
+                        <60 - E Grade
+                                        */
+        int mark;
+
+            System.out.println("Enter the mark");
+            mark = sc.nextInt();
+
+            if (mark>=90) {
+                System.out.println(mark +" "+"is A Grade");
+                }
+            else if (mark>=80) {
+                System.out.println(mark + " " + "is a B Grade");
+            }
+            else if (mark>=70){
+                System.out.println(mark + " "+ "is C Grade");
+            }
+            else if (mark>=60){
+                System.out.println(mark + " "+ "is D Grade");
+            }
+            else{
+                System.out.println(mark + " "+ "is E Grade");
+            }
+
+
+
+
     }
         
 }
-}
+
