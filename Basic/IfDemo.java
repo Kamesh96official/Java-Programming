7f5ceb8e87abc799d483b005fafe86ce294b0be2
kamesh96official <cameociscoz@gmail.com>
2026-03-14 21:41:48 +0530

    If else

--- Basic/IfDemo.java
+++ Basic/IfDemo.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class IfDemo{
 public static void main (String args[]){
 
-    
+    int num;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number");
+    num =sc.nextInt();
+
+    if (num>0) {
+        System.out.println(num +" " +"The number is positive");
+        
+    }
+    else{
+
+        if( num ==0){
+            System.out.println(num + " "+ "The number is Neutral");
+        }
+        else{
+        System.out.println(num  +" "+ "The number is negative");
+        }
+    }
+        
 }
 }
