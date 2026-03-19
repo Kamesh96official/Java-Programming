f36534041ca15801eae049490694f5d34114ee80
Kameshwaran P <kamesh-13187@kamesh-13187.csez.zohocorpin.com>
2026-03-19 21:44:41 +0530

    Loops

---
+++ Basic/loops.java
@@ -0,0 +1,12 @@
+public class loops{
+    public static void main (String args []){
+
+        int i;
+
+        for(i=1;i<=5;i++){
+            System.out.println("*");
+        }
+        System.out.println("The value of i is:" + i);
+
+    }
+}
