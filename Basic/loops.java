52f98d3b4e8370172e63882ca6f922e4caf2a380
kamesh96official <cameociscoz@gmail.com>
2026-03-21 20:58:37 +0530

    Loops Exercise

--- Basic/loops.java
+++ Basic/loops.java
@@ -8,5 +8,22 @@ public class loops{
         }
         System.out.println("The value of i is:" + i);
 
-    }
+        
+        int j=1;
+
+        while (j<=5) {
+            
+            System.out.println("*");
+            j++;
+            }
+        System.out.println(" The value of j is :" + j);
+
+        int k=1;
+
+        do{
+            System.out.println("*");
+            k++;
+            } while(k<=5);
+            System.out.println("The value of k is: " + k);
+}
 }
